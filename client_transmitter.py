1bae6e2d9622969cd6add44287589d2e66ef0f2e
Yousefi-ai <faranakyousefiai@gmail.com>
2025-09-14 18:42:03 +0330

    Add files via upload

---
+++ client_transmitter.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import time
+import torch 
+
+class Transmitter : 
+    def __init__(self,server_url , device) : 
+        self.server_url = server_url #" https://d5e33cbc658b.ngrok-free.app/is_even"
+        self.device = device
+    def send_data(self , x , label ,  status  ): #status if is train or test 
+        x = x.detach().cpu().tolist()
+        label = label.detach().cpu().tolist()
+        if status == 'train' : 
+            data = {
+                'prediction_iput':x,
+                'label': label ,
+                'status' : status
+            }
+        elif status == 'test' : 
+            data = {
+                'prediction_iput':x,
+                'label': [] , 
+                'status' : status
+            }
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(self.server_url, data=json.dumps(data), headers=headers)
+        response.raise_for_status()
+        result = response.json()
+        if status == 'train' : # result = {'grad' :  }
+            grad = result['grad']
+            return torch.tensor(grad).to(self.device)
+        elif status == 'test' :
+            prediction = torch.tensor(result['prediction']).to(self.device)
+            return  prediction
+
